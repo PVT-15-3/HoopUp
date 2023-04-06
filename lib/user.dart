73e43bc79070306481a4df4934827a192121821a
Simon Fredriksson <simon.fredriksson93@gmail.com>
2023-04-06 12:06:51 +0200

    user.dart
    test 2

--- lib/user.dart
+++ lib/user.dart
@@ -1,4 +1,3 @@
-//Hej hej :)
 class User {
   String username;
   int skillLevel;
@@ -7,7 +6,18 @@ class User {
 
   User(this.username, this.skillLevel, this.id, this.email) {
     if (skillLevel < 0 || skillLevel > 5) {
-      throw ArgumentError('Skill level must be between 0 and 10');
+      throw ArgumentError('Skill level must be between 0 and 5');
     }
   }
+  // getters and setters for the class properties 
+  getUsername() => username;
+  getSkillLevel() => skillLevel;
+  setSkillLevel(skillLevel) { 
+    if (skillLevel < 0 || skillLevel > 5) {
+      throw ArgumentError('Skill level must be between 0 and 5');
+    } this.skillLevel = skillLevel;
+  }
+  getId() => id;
+  getEmail() => email;
+  setEmail(String email) => this.email = email;
 }
