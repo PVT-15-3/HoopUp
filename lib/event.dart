42817bcefd9c1b6042d27ab871694cb984d0a996
Simon Fredriksson <simon.fredriksson93@gmail.com>
2023-04-06 12:12:41 +0200

    first commit
    Event class

---
+++ lib/event.dart
@@ -0,0 +1,12 @@
+class Event {
+final String name;
+final String description; 
+final String id;
+
+Event(this.name, this.description, this.id);
+
+// getters for the class properties
+getName() => name;
+getDescription() => description;
+getId() => id;
+}
