2ecbbb4dda2438d1719c4f4c5aed0c80adc2702d
Latmask <105066980+Latmask@users.noreply.github.com>
2023-04-09 21:16:02 +0200

    fixed chat, event and message

--- lib/classes/event.dart
+++ lib/classes/event.dart
@@ -1,20 +1,16 @@
 class Event {
-final String name;
-final String description; 
-final String id;
+  final String name;
+  final String description;
+  final String id;
 
-Event(this.name, this.description, this.id);
+  Event(this.name, this.description, this.id);
 
 // getters for the class properties
-getName() => name;
-getDescription() => description;
-getId() => id;
+  getName() => name;
+  getDescription() => description;
+  getId() => id;
 
-Map<String, dynamic> toJson() {
-    return {
-      'name': name,
-      'description': description,
-      'id': id
-    };
+  Map<String, dynamic> toJson() {
+    return {'name': name, 'description': description, 'id': id};
   }
 }
