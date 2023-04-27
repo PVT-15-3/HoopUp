d925ea769fefe40f0f1fc5c91c7bd80807a97ee9
Erik Zubarev <45092431+SirSpankexe@users.noreply.github.com>
2023-04-27 21:30:06 +0200

    Update hoopup_user_test.dart

--- test/hoopup_user_test.dart
+++ test/hoopup_user_test.dart
@@ -1,43 +1,58 @@
+import 'dart:math';
+
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
 import 'package:my_app/classes/hoopup_user.dart';
 
-class MockUser extends Mock implements HoopUpUser {}
-
 void main() {
-  final HoopUpUser user1 = MockUser();
-  final HoopUpUser user2 = MockUser();
-  final HoopUpUser user3 = MockUser();
+  late HoopUpUser userGeoff;
+  late HoopUpUser userGeoffClone;
+  late HoopUpUser userAubrey;
 
   setUp(() {
-    when(() => user1.username).thenReturn("Geoff");
-    when(() => user1.skillLevel).thenReturn(5);
-    when(() => user1.gender).thenReturn("Male");
-    when(() => user1.id).thenReturn("id1");
-    when(() => user1.photoUrl)
-        .thenReturn("https://i.redd.it/gjymk14blvua1.jpg");
-
-    when(() => user2.username).thenReturn("Kim");
-    when(() => user2.skillLevel).thenReturn(1);
-    when(() => user2.gender).thenReturn("Other");
-    when(() => user2.id).thenReturn("id2");
-    when(() => user2.photoUrl)
-        .thenReturn("https://i.redd.it/lxiaa9ofd1271.png");
-
-    when(() => user3.username).thenReturn("Aubrey");
-    when(() => user3.skillLevel).thenReturn(2);
-    when(() => user3.gender).thenReturn("Female");
-    when(() => user3.id).thenReturn("id3");
-    when(() => user3.photoUrl)
-        .thenReturn("https://i.redd.it/m3769x8h0py81.jpg");
+    userGeoff = HoopUpUser(
+        username: "Geoff",
+        skillLevel: 5,
+        id: "id1",
+        photoUrl: "https://i.redd.it/gjymk14blvua1.jpg",
+        gender: "Male");
+    userGeoffClone = HoopUpUser(
+        username: "Geoff",
+        skillLevel: 5,
+        id: "id1",
+        photoUrl: "https://i.redd.it/gjymk14blvua1.jpg",
+        gender: "Male");
+    userAubrey = HoopUpUser(
+        username: "Aubrey",
+        skillLevel: 3,
+        id: "id2",
+        photoUrl: "https://i.redd.it/m3769x8h0py81.jpg",
+        gender: "Female");
   });
 
-  //tests
-  group("Testing variables", () {
-    test("Testing usernames", () async {
-      expect(user1.username, "Geoff");
-      expect(user2.username, "Kim");
-      expect(user3.username, "Aubrey");
+  group("Testing event list changing operations", () {
+    test("Initial state of events list is empty", () {
+      expect(userGeoff.events, []);
     });
+    test("Adding one event and seeing it in the list", () {
+      userGeoff.addEvent("event1");
+      expect(userGeoff.events, ["event1"]);
+    });
+    test("Adding same event multiple times should give return false", () {
+      //TODO allow for add event to return true or false
+      //expect(userAubrey.addEvent("event2"), true);
+      //expect(userAubrey.addEvent("event2"), false);
+      userAubrey.addEvent("event2");
+      userAubrey.addEvent("event2");
+      expect(userAubrey.events, ["event2"]);
+    });
+
+    test("Removing an event from a user", () {});
+    test(
+        "Removing and adding events in different orders seeing if it's consistent in the end",
+        () {});
+    test("Removing the same event twice should return false", () => null);
   });
+
+  group("Testing equalisation, toString, hashing and validation functions",
+      () {});
 }
