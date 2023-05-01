bb54dd6b712f201ad150374e519b98dc662ad87c
Simon Fredriksson <simon.fredriksson93@gmail.com>
2023-05-01 13:24:46 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/handlers/event_handler.dart';
 import 'package:my_app/pages/create_event.dart';
-import 'package:my_app/pages/joined_events_pages.dart';
+import 'package:my_app/pages/list_events.dart%20';
+import 'package:my_app/pages/main_menu.dart';
 import 'package:my_app/pages/sign_up_page.dart';
 import 'package:provider/provider.dart';
 import 'pages/map.dart';
@@ -10,8 +10,7 @@ import 'pages/profile_page.dart';
 import 'services/firebase_options.dart';
 import 'providers/hoopup_user_provider.dart';
 import 'pages/log_in_page.dart';
-
-import 'classes/bottom_nav_bar.dart';
+import 'pages/join_event_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -35,15 +34,15 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.orange,
         ),
-        home: const BottomNavBar(),
+        home: const MainMenu(),
         routes: {
           '/map': (context) => const Map(),
           '/create_event.dart': (context) => const CreateEventPage(),
           '/sign_up_page.dart': (context) => SignUpPage(),
           '/log_in_page.dart': (context) => LogInPage(),
           '/profile_page.dart': (context) => const ProfilePage(),
-          '/list_events.dart': (context) => const JoinedEventsPage(showJoinedEvents : true),
-          '/join_event_page.dart': (context) => const EventHandler(showJoinedEvents: false),
+          '/list_events.dart': (context) => const ListEventsPage(showJoinedEvents : true),
+          '/join_event_page.dart': (context) => const JoinEventPage(showJoinedEvents: false),
         },
       ),
     );
