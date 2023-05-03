fbc21f9f6aee1144a3fab39f897aa823c3d0f9b6
nidorbRotkiv <viktor.karl.brodin.1@gmail.com>
2023-05-03 20:41:01 +0200

    card in list shows how many people have joined a game
    also some refactoring of event_list_item.dart

--- lib/widgets/event_list_item.dart
+++ lib/widgets/event_list_item.dart
@@ -25,6 +25,10 @@ class EventListItem extends StatefulWidget {
 class _EventListItemState extends State<EventListItem> {
   late Future<bool> _hasUserJoined;
   late Event _event;
+  late Map _eventMap;
+  late List<String>? _userIdsList;
+  late int _numberOfPlayersInEvent;
+  final User _firebaseUser = FirebaseAuth.instance.currentUser!;
 
   @override
   void initState() {
@@ -35,8 +39,7 @@ class _EventListItemState extends State<EventListItem> {
 
   // Added type annotations for variables and return type
   Future<bool> _checkJoined() async {
-    final User? user = FirebaseAuth.instance.currentUser;
-    final Map userMap = await getMapFromFirebase("users", user!.uid);
+    final Map userMap = await getMapFromFirebase("users", _firebaseUser.uid);
     final List<dynamic> eventsList = userMap['events'] ?? [];
     return eventsList.contains(_event.id);
   }
@@ -61,75 +64,85 @@ class _EventListItemState extends State<EventListItem> {
           // Otherwise, get the value of the joined variable from the snapshot data
           final bool hasUserJoined = snapshot.data ?? false;
           // Build a Row widget with the event information and a button to join/cancel the event
-          return Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 8.0),
-                  child: ListTile(
-                    title: Text(_event.name),
-                    subtitle: Text(_getSubtitleText()),
-                  ),
-                ),
-              ),
-              TextButton(
-                onPressed: () async {
-                  // Call the handleEvent function to join or cancel the event, and update the state to rebuild the UI
-                  await _toggleEvent(hasUserJoined);
-                  setState(() {
-                    _hasUserJoined = _checkJoined();
-                  });
-                },
-                child: Text(
-                  hasUserJoined ? 'Cancel event' : 'Join event',
-                  style: const TextStyle(
-                    fontSize: 18.0,
-                  ),
-                ),
-              )
-            ],
-          );
+          return FutureBuilder<bool>(
+              future: loadDataFromFirebase(),
+              builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const SizedBox.shrink();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  return Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child: ListTile(
+                            title: Text(_event.name),
+                            subtitle: Text(_getSubtitleText()),
+                          ),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () async {
+                          // Call the handleEvent function to join or cancel the event, and update the state to rebuild the UI
+                          await _toggleEvent(hasUserJoined);
+                          setState(() {
+                            _hasUserJoined = _checkJoined();
+                          });
+                        },
+                        child: Text(
+                          hasUserJoined ? 'Cancel event' : 'Join event',
+                          style: const TextStyle(
+                            fontSize: 18.0,
+                          ),
+                        ),
+                      )
+                    ],
+                  );
+                }
+              });
         }
       },
     );
+  }
+
+  Future<bool> loadDataFromFirebase() async {
+    _eventMap = await getMapFromFirebase("events", _event.id);
+    _userIdsList = List.from(_eventMap['userIds'] ?? []);
+    _numberOfPlayersInEvent = _userIdsList!.length;
+    return true;
   }
 
   String _getSubtitleText() {
     return 'Event info: ${_event.description}\n'
         'Date and time: ${_event.time.getFormattedStartTime()}'
         '\nThe event is for: ${_event.genderGroup}\n'
-        'Participants: ${_event.playerAmount}\n'
+        'Participants: $_numberOfPlayersInEvent / ${_event.playerAmount}\n'
         'Skill level: ${_event.skillLevel}';
   }
 
   Future<void> _toggleEvent(bool hasUserJoined) async {
-    // Get the currently logged-in user's ID
-    final String? userId = FirebaseAuth.instance.currentUser?.uid;
-    Map? eventMap = await getMapFromFirebase("events", _event.id);
-    Map? userMap = await getMapFromFirebase("users", userId!);
-
-    List<String>? userIdsList = List.from(eventMap['userIds'] ?? []);
+    Map? userMap = await getMapFromFirebase("users", _firebaseUser.uid);
     List<String> eventsList = List.from(userMap['events'] ?? []);
-
-    int numberOfPlayersInEvent = userIdsList.length;
-    int numberOfAllowed = eventMap['playerAmount'];
     if (!mounted) {
       return;
     }
     HoopUpUserProvider userProvider =
         Provider.of<HoopUpUserProvider>(context, listen: false);
 
-    if (numberOfPlayersInEvent >= numberOfAllowed && !hasUserJoined) {
+    if (_numberOfPlayersInEvent >= _event.playerAmount && !hasUserJoined) {
       showCustomToast("This event is full!", Icons.error, context);
       return;
     }
 
     if (eventsList.contains(_event.id)) {
-      removeUserFromEvent(
-          _event.id, eventsList, userId, userIdsList, userProvider);
+      removeUserFromEvent(_event.id, eventsList, _firebaseUser.uid,
+          _userIdsList!, userProvider);
       return;
     } else {
-      addUserToEvent(_event.id, eventsList, userId, userIdsList, userProvider);
+      addUserToEvent(_event.id, eventsList, _firebaseUser.uid, _userIdsList!,
+          userProvider);
       return;
     }
   }
