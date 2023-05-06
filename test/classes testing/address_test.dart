308227393cd94953054d2be6aac0213fd6b35285
Erik Zubarev <45092431+SirSpankexe@users.noreply.github.com>
2023-05-06 12:40:10 +0200

    Formatting

--- test/classes testing/address_test.dart
+++ test/classes testing/address_test.dart
@@ -4,16 +4,12 @@ import 'package:my_app/classes/address.dart';
 void main() {
   late Address sut;
 
-  setUp(() {});
-
-  arrangeValidAddress() {
+  setUp(() {
     sut = Address('123 Main St', 'Anytown', 12345, 12.345, 67.890);
-  }
+  });
 
   group('Address Tests', () {
     test('toJson should return a valid map', () {
-      arrangeValidAddress();
-
       final json = sut.toJson();
 
       expect(json['street'], equals('123 Main St'));
@@ -24,16 +20,12 @@ void main() {
     });
 
     test('toString should return a formatted string', () {
-      arrangeValidAddress();
-
       final string = sut.toString();
 
       expect(string, equals('123 Main St, 12345\nAnytown'));
     });
 
     test('Getters should return correct values', () {
-      arrangeValidAddress();
-
       expect(sut.street, equals('123 Main St'));
       expect(sut.city, equals('Anytown'));
       expect(sut.postalCode, equals(12345));
---
+++ test/pages testing/profile_page_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:my_app/classes/hoopup_user.dart';
+import 'package:my_app/pages/log_in_page.dart';
+import 'package:my_app/pages/profile_page.dart';
+import 'package:my_app/providers/hoopup_user_provider.dart';
+import 'package:provider/provider.dart';
+
+void main() {}
