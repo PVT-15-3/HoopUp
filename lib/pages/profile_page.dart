e1c8838b90af12d31e4e12402c27b7a54c6e5f6b
Erik Zubarev <45092431+SirSpankexe@users.noreply.github.com>
2023-05-12 15:38:53 +0200

    Update profile page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -2,12 +2,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:my_app/classes/hoopup_user.dart';
 import 'package:my_app/pages/log_in_page.dart';
+import 'package:my_app/widgets/editable_fields_for_profile_and_login_pages.dart';
 import 'package:my_app/widgets/toaster.dart';
 import 'package:provider/provider.dart';
 import '../providers/hoopup_user_provider.dart';
 
 class ProfilePage extends StatefulWidget {
-  ProfilePage({Key? key}) : super(key: key);
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
@@ -15,10 +16,6 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   bool isEditable = false;
-  TextEditingController nameController = TextEditingController();
-  TextEditingController emailController = TextEditingController();
-  TextEditingController dobController = TextEditingController();
-  TextEditingController genderController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -86,7 +83,12 @@ class _ProfilePageState extends State<ProfilePage> {
                           initialValue: email,
                           decoration: const InputDecoration(
                             labelText: "E-mail",
-                            border: OutlineInputBorder(),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                //TODO change color
+                                color: Colors.orange,
+                              ),
+                            ),
                             contentPadding: EdgeInsets.symmetric(
                                 horizontal: 10, vertical: 0),
                           ),
@@ -152,102 +154,7 @@ class _ProfilePageState extends State<ProfilePage> {
           elevation: 0.0,
           backgroundColor: Colors.white,
         ),
-        body: Center(
-          child: Consumer<HoopUpUserProvider>(
-            builder: (context, userProvider, child) {
-              HoopUpUser user = userProvider.user!;
-              String username = user.username;
-              int skillLevel = user.skillLevel;
-              String gender = user.gender;
-              int age = user.age;
-
-              final controller = TextEditingController(text: username);
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  //Edit picture
-                  ElevatedButton(
-                    onPressed: () async {
-                      user.pickProfilePicture();
-                    },
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.transparent),
-                      elevation: MaterialStateProperty.all<double>(0),
-                    ),
-                    child: Column(
-                      children: const [
-                        CircleAvatar(
-                          radius: 50,
-                          //TODO Fix photo URL
-                          backgroundImage: AssetImage(""),
-                        ),
-                        Text("Edit picture"),
-                      ],
-                    ),
-                  ),
-                  //Edit skill level
-                  const SizedBox(height: 40),
-
-                  Container(
-                      margin: const EdgeInsets.fromLTRB(80, 0, 80, 0),
-                      child: Column(children: [
-                        //Edit First name
-                        TextField(
-                          controller: controller,
-                          decoration: InputDecoration(
-                            border: const OutlineInputBorder(),
-                            labelText: 'Username',
-                            hintText: username,
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-
-                        //Edit email
-
-                        //Edit age
-                        TextField(
-                          controller: controller,
-                          decoration: InputDecoration(
-                            border: const OutlineInputBorder(),
-                            labelText: "Age",
-                            hintText: "$age",
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-
-                        //Edit gender
-                        TextField(
-                          controller: controller,
-                          decoration: InputDecoration(
-                            border: const OutlineInputBorder(),
-                            labelText: 'Gender',
-                            hintText: gender,
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                      ])),
-
-                  ElevatedButton(
-                    onPressed: () {
-                      user.skillLevel = skillLevel;
-                      user.gender = gender;
-                      if (controller.text.length >= 3) {
-                        user.username = controller.text;
-                        showCustomToast(
-                            "Profile updated", Icons.person, context);
-                      } else {
-                        showCustomToast(
-                            "Username is too short", Icons.warning, context);
-                      }
-                    },
-                    child: const Text('Save'),
-                  ),
-                ],
-              );
-            },
-          ),
-        ),
+        body: const EditableFields(),
       );
     }
   }
