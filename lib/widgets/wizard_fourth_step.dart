20c7137b8112e488dbe532333e0c401b574be4c9
nidorbRotkiv <viktor.karl.brodin.1@gmail.com>
2023-05-22 19:24:08 +0200

    made confirmation text before creating event more readable

--- lib/widgets/wizard_fourth_step.dart
+++ lib/widgets/wizard_fourth_step.dart
@@ -81,10 +81,10 @@ class WizardFourthStep extends StatelessWidget {
                               '${wizardProvider.court?.name}\n'
                               '${wizardProvider.eventDate.year}-${wizardProvider.eventDate.month.toString().padLeft(2, '0')}-${wizardProvider.eventDate.day.toString().padLeft(2, '0')}\n'
                               'Start time: ${wizardProvider.eventStartTime.hour.toString()}:${wizardProvider.eventStartTime.minute.toString().padLeft(2, '0')}\n'
-                              'End Time: ${wizardProvider.eventEndTime.hour.toString()}:${wizardProvider.eventEndTime.minute.toString().padLeft(2, '0')}\n'
+                              'End time: ${wizardProvider.eventEndTime.hour.toString()}:${wizardProvider.eventEndTime.minute.toString().padLeft(2, '0')}\n'
                               '\nFor ${wizardProvider.numberOfParticipants} players\n'
-                              '${wizardProvider.selectedGender} between ${wizardProvider.minimumAge} - ${wizardProvider.maximumAge}\n'
-                              'with skill level\n',
+                              '${wizardProvider.selectedGender} between ${wizardProvider.minimumAge}-${wizardProvider.maximumAge} years old\n'
+                              'with skill level:\n',
                           style: const TextStyle(
                             fontFamily: 'Open Sans',
                             fontStyle: FontStyle.normal,
